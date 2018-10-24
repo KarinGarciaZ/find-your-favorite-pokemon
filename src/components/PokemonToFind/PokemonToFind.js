53ed27d489d431ae35b14336774adb7a17125fd8
KarinGarciaZ <oscarin962011@hotmail.com>
2018-10-23 23:42:11 -0500

    Input to search pokemons added.

--- src/components/PokemonToFind/PokemonToFind.js
+++ src/components/PokemonToFind/PokemonToFind.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const pokemonToFind = ( props ) => {
   return(
-    <div>
+    <div className='row'>
+      <div className='col-3'>
 
+      </div>
+      <div className='col-6'>
+        <input type='text' className="form-control" placeholder='Find your pokemon by its Name or ID...' value={ props.name } onChange={ props.valueChanged }/>
+      </div>
+      <div className='col-3'>
+        <button className='btn btn-primary' onClick={props.clicked}>Find</button>
+      </div>
     </div>
   )
 }
