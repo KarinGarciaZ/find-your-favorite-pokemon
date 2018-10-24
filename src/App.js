629ed349a2cf0ce934a6a6ef4a27c2605225aa2a
KarinGarciaZ <oscarin962011@hotmail.com>
2018-10-24 00:58:43 -0500

    Router applied to get into the application even if url isnt well at all.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import FindPokemon from '../src/containers/FindPokemon/FindPokemon';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <FindPokemon>
-      
-      </FindPokemon>
+      <BrowserRouter>
+        <Route path="/" component={FindPokemon}/>
+      </BrowserRouter>
     );
   }
 }
