4e2f9701b9673da7a75a8e92798e721b0545b8f6
Oscar Garcia <oscar.garcia@unosquare.com>
2018-11-22 10:44:20 -0800

    Adding new pokemon cards dinamicly.

--- src/components/PokemonImages/PokemonImages.js
+++ src/components/PokemonImages/PokemonImages.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 class PokemonImages extends Component {
+
+  componentDidMount() {
+    
+  }
+
   render() {
+    let cards = this.props.pokemons.map( eachPokemon => {
+      return <div key={eachPokemon.id} className="cards">
+              <div className="cards__picture">        
+                <img src={eachPokemon.img} alt="MyImage" />          
+              </div>
+              <div className="cards__details">
+                <ul>
+                  <li>Pokedex Number <strong>{eachPokemon.id}</strong></li>
+                  <li>{eachPokemon.name}</li>
+                </ul>
+              </div>
+            </div>     
+    } )
+    console.log(this.props.pokemons)
     return(
-      <div className='images-section'>
-        <div className='images'>
-
-        </div>
-      </div>
-
+      <div className="container-flex">
+        { cards }
+      </div>    
     )
   }
 }
